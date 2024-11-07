c84a0bd8006fd878ff6e818e46348fe8a853e44d
Gabriel <gcr2@outlook.com.br>
2024-11-07 20:59:16 -0300

    Update ContaCorrete.py

--- ContaCorrete.py
+++ ContaCorrete.py
@@ -1,2 +1,4 @@
 class ContaCorrente(Conta):
-    def __init__(self, numero)
+    def __init__(self, numero, limiteCredito):
+        super().__init__(numero)
+        self.__limiteCredito = limiteCredito

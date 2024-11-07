4d65fc6e3fe4b816dda9509483838d4ec659ffae
Gabriel <gcr2@outlook.com.br>
2024-11-07 16:11:57 -0300

    Update Conta.py

--- Conta.py
+++ Conta.py
@@ -1,1 +1,18 @@
+class Conta:
+    def __init__(self, numero, saldo):
+        self.__numero = numero
+        self.__saldo = saldo
 
+    #getters
+    def getNumero(self):
+        return self.__numero
+
+    def getSaldo(self):
+        return self.__saldo
+
+    #setters
+    def setNumero(self, numero):
+        return self.__numero = saldo
+
+    def setSaldo(self, saldo):
+        return self.__saldo = saldo
